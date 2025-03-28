121048cddf79dd859777bab998d60e648c168768
安逐悲 <38884950+NayamiOR@users.noreply.github.com>
2025-03-28 17:16:32 +0000

    mo

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,11 +3,9 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    for i in input.
 }
 
 fn compose_me(input: &str) -> String {
@@ -17,7 +15,7 @@ fn compose_me(input: &str) -> String {
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.
 }
 
 #[cfg(test)]
